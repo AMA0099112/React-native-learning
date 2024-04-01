fb3412ed89562d593bf3685859180f68caad9a36
AMA0099112 <99637348+AMA0099112@users.noreply.github.com>
2024-04-02 00:53:56 +0800

    使用flatlist 渲染title

--- learnwk4/App.js
+++ learnwk4/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import Header from "./src/components/Header";
 import Albumlist from "./src/components/AlbumList";
 import albumData from "./src/json/albums.json";
-import { SafeAreaView, StatusBar } from "react-native";
+import { SafeAreaView, StatusBar, Text } from "react-native";
+import { FlatList } from "@gluestack-ui/themed";
 
 const App = () => {
   return (
     <SafeAreaView flex={1}>
       <StatusBar />
       <Header title={albumData.albumTitle} />
-      <Albumlist />
+      <FlatList
+        data={albumData.albumList}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+        keyExtractor={item => item.title}
+      />
     </SafeAreaView>
   )
 }
