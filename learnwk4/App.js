545e045a87d436c83940ab7391a9102b4b741d0b
AMA0099112 <99637348+AMA0099112@users.noreply.github.com>
2024-04-02 00:11:08 +0800

    use another props

--- learnwk4/App.js
+++ learnwk4/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Header from "./src/components/Header";
 import Albumlist from "./src/components/AlbumList";
-import albumData from "./src/components/AlbumList";
+import albumData from "./src/json/albums.json";
 import { SafeAreaView, StatusBar } from "react-native";
-
 
 const App = () => {
   return (
     <SafeAreaView flex={1}>
       <StatusBar />
-      <Header title={albumData.title} />
+      <Header title={albumData.albumTitle} />
       <Albumlist />
     </SafeAreaView>
   )
--- learnwk4/src/components/Header.js
+++ learnwk4/src/components/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-
-const Header = ({ title }) => {
+//props 參數 從json檔匯入
+const Header = (props) => {
+    const title = props.title
     return (
         <View style={styles.headerStyle}>
             <Text style={styles.textstyle}>{title}</Text>
@@ -11,7 +12,20 @@ const Header = ({ title }) => {
 }
 
 const styles = StyleSheet.create({
-
+    headerStyle: {
+        backgroundColor: "F8F8F8",
+        justifyContent: "center",
+        alignContent: "center",
+        height: 60,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        //Android Only
+        elevation: 2
+    },
+    textstyle: {
+        fontSize: 20,
+    }
 });
 
 
