d82053b0bc02170b735055a49e7445c6786803c7
AMA0099112 <99637348+AMA0099112@users.noreply.github.com>
2024-04-02 23:27:32 +0800

    修改 Tab 的顏色

--- learnwk4/src/navigation/index.js
+++ learnwk4/src/navigation/index.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AlbumScreen from "../screens/AlbumScreen";
 import SettingsScreen from "../screens/SettingsScreen";
 import DetailScreen from "../screens/DetailScreen";
-
+import MyTheme from "../theme";
 import albumData from "../json/albums.json"
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
 
 const Navigation = () => {
     return (
-        <NavigationContainer>
+        <NavigationContainer theme={MyTheme}>
             {/* 設定要跳轉的頁面並寫成一個函式 */}
             {/* <StackNavigatior /> */}
             <MyTab />
@@ -38,21 +39,41 @@ const HomeStack = () => {
 };
 
 const MyTab = () => {
+    // 設定顏色
+    const { colors } = useTheme();
     return (
         <Tab.Navigator
             // 將stack函數的名稱放進來就可以使用功能
             initialRouteName="HomeStack"
             screenOptions={{
-                tabBarActiveTintColor: '#e91e63'
+                tabBarActiveTintColor: colors.primary700,
+                tabBarInactiveTintColor: colors.light500
             }}
         >
             <Tab.Screen
                 name="HomeStack"
                 component={HomeStack}
+                options={{
+                    headerShown: false,
+                    title: "Home",
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={26} />
+                    ),
+                }}
             />
             <Tab.Screen
                 name="Setting"
                 component={SettingsScreen}
+                options={{
+                    title: "Settings",
+                    headerTitleStyle: {
+                        fontWeight: '400',
+                        fontSize: 20
+                    },
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="cog" color={color} size={26} />
+                    ),
+                }}
             />
         </Tab.Navigator>
     )
