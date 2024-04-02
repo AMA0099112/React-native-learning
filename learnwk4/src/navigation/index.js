97a0e8bbefb307be5ebf7a48605bd3fa69a735aa
AMA0099112 <99637348+AMA0099112@users.noreply.github.com>
2024-04-02 23:13:02 +0800

    增加 Tab

--- learnwk4/src/navigation/index.js
+++ learnwk4/src/navigation/index.js
@@ -1,23 +1,27 @@
 import React from "react";
-
-
-import AlbumScreen from "../screens/AlbumScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AlbumScreen from "../screens/AlbumScreen";
+import SettingsScreen from "../screens/SettingsScreen";
 import DetailScreen from "../screens/DetailScreen";
 
+import albumData from "../json/albums.json"
+
 const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
 
 const Navigation = () => {
     return (
         <NavigationContainer>
             {/* 設定要跳轉的頁面並寫成一個函式 */}
-            <StackNavigatior />
+            {/* <StackNavigatior /> */}
+            <MyTab />
         </NavigationContainer>
     );
 }
 
-const StackNavigatior = () => {
+const HomeStack = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen
@@ -31,6 +35,27 @@ const StackNavigatior = () => {
         </Stack.Navigator>
     );
 
+};
+
+const MyTab = () => {
+    return (
+        <Tab.Navigator
+            // 將stack函數的名稱放進來就可以使用功能
+            initialRouteName="HomeStack"
+            screenOptions={{
+                tabBarActiveTintColor: '#e91e63'
+            }}
+        >
+            <Tab.Screen
+                name="HomeStack"
+                component={HomeStack}
+            />
+            <Tab.Screen
+                name="Setting"
+                component={SettingsScreen}
+            />
+        </Tab.Navigator>
+    )
 }
 
 export default Navigation;
---
+++ learnwk4/src/screens/SettingsScreen.js
@@ -0,0 +1,14 @@
+import { Center, Text } from "@gluestack-ui/themed";
+import React from "react";
+
+const SettingsScreen = () => {
+    return (
+        <Center bg="emerald.100" flex={1}>
+            <Text fontSize={30}>
+                This is a Setting Page.
+            </Text>
+        </Center>
+    )
+}
+
+export default SettingsScreen;
